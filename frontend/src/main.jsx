c01d44ec5a3199330ad041656ab74d371fb47a94
Salman Dabbakuti <29351207+Salmandabbakuti@users.noreply.github.com>
2024-10-28 10:54:27 +0530

    chore: few cleanups

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import SiteLayout from "./components/SiteLayout.jsx";
-import Web3Provider from "./components/Web3Provider.jsx";
-import App from "./App.jsx";
+import SiteLayout from "./components/SiteLayout";
+import Web3Provider from "./components/Web3Provider";
+import App from "./App";
 
 import "./index.css";
 
